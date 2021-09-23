7027c66ba66ae93a6a239d40913ac7effb0fa7c5
khalil192 <shaikkhalil192@gmail.com>
2021-09-23 21:01:32 +0530

    added bitTree

--- awesome ds/binary_index_trees/binary_index_tree_sample.cpp
+++ awesome ds/binary_index_trees/binary_index_tree_sample.cpp
@@ -4,46 +4,84 @@ fenwick trees use case :
 benifits ..........
 */
 
-#include<stdio.h>
 #include<iostream>
+#include<string.h>
 #include<vector>
+#include<math.h>
+#include<queue>
 #include<stack>
+#include<algorithm>
+#include<iterator>
+#include<map>
+#include <sstream>
+#include<set>
+#define lli long long int
+#define mp make_pair
+#define ULTRA_INSTINCT ios_base::sync_with_stdio(false); cin.tie(0)
+#define fo(i,n) for(lli i=0;i<(n);i++)
+#define FO(i,x,n) for(lli i=(x);i<(n);i++)
+#define pb push_back
+#define vi vector<lli>
+#define pi pair<lli,lli>
+#define vii vector<pi >
+#define Endl "\n"
+#define ITA 200005
+#define ROH 200003
+#define MAXI 200003
 using namespace std;
-#define MAXI 1000
+lli mod = 1e9+7 ;
+lli gcd(lli a, lli b){
+    if (b == 0) return a;
+    return gcd(b, a % b); 
+}
 /* 
 fenwick trees are 1 based indexed array...
 note that the original array should be 1 based indexed as well as the tree
+
 */
-int original_arr[MAXI],Btree[MAXI],n;
+class bitree{
+    public :
+    vi tree; 
+    lli n;
+    bitree(vi vec){ // pass a one based array
+        n = vec.size()-1; // why -1 ? 
+        tree = vec;
+        initialise();
+    }
+    bitree(lli num){
+        n = num;
+        tree.resize(n+1,0); // look at this
+    }
+    void initialise(){
+        FO(i,1,n+1){
+            int j = i+lsb(i);
+            if(j<=n){
+                tree[j] += tree[i];
+            }
+        }
+    }
+    lli lsb(lli i){ return i&-i;    }
 
-int lsb(int i){
-    return i&-i;
-}
+    lli prefix_query(lli i){
+        lli sum = 0;
+        while(i>0){
+            sum += tree[i];
+            i -= lsb(i);
+        }
+        return sum;
+    }
+    lli range_query(lli i,lli j){ 
+        return prefix_query(j) - prefix_query(i-1);
+    }
+    void update(int i,int val){
+        while(i<=n){
+            tree[i] += val;
+            i+=lsb(i);
+        }
+    }
 
-void initialise(){
-for(int i=0;i<n;i++)    Btree[i] = original_arr[i];
-for(int i=1;i<=n;i++){
-    int j = i+lsb(i);
-    if(j<=n)    Btree[j] += Btree[i];
-}
-}
-int prefix_query(int i){
-    int sum = 0;
-    while(i>0){
-        sum += Btree[i];
-        i -= lsb(i);
-    }
-    return sum;
-}
-int range_query(int i,int j){ 
-    return prefix_query(j) - prefix_query(i-1);
-    }
-void update(int i,int val){
-     while(i<=n){
-        Btree[i] += val;
-        i+=lsb(i);
-    }
-}
+};
+
 
 int main(){
 
