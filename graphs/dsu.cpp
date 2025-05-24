448cc7a2de15b8679daaa4804ae17547673e8388
khalil192 <khalil@SIMPL-ENG-049.local>
2025-05-24 14:21:02 +0530

    add-dsu

--- graphs/dsu.cpp
+++ graphs/dsu.cpp
@@ -1,17 +1,45 @@
 #include<stdio.h>
 #include<iostream>
+#include<vector>
 using namespace std;
-int i;
-int foo ( int x )
-{
-        if ( x <= 0 )
-                return 0;
-        else
-                return foo(x-2) + x;
-}
 
-int main()
-{
-        cout << foo(8);
+
+
+class DSU{ 
+public:
+    vector<int> parent, size;
+    DSU(int n){
+        parent.resize(n);
+        size.resize(n);
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+ 
+    int root(int i){
+        while(parent[i] != i){
+            parent[i] = parent[parent[i]];
+            i = parent[i];
+        }
+        return i;
+    }
+ 
+    void join(int a, int b){
+        a = root(a);
+        b = root(b);
+        if(a != b){
+            if(size[a] < size[b]){
+                swap(a, b);
+            }
+            parent[b] = a;
+            size[a] += size[b];
+        }
+    }
+};
+ 
+
+
+int main(){
         return 0;
 }
